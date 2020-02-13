67d27431edd955215ddc90a581dd94b6d056fd08
Avec <avec112@gmail.com>
2020-02-13 15:07:03 +0100

    Initial commit

---
+++ src/main/java/io/avec/outputtracker/OutputTrackerApplication.java
@@ -0,0 +1,13 @@
+package io.avec.outputtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutputTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OutputTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/avec/outputtracker/OutputTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.avec.outputtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutputTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
