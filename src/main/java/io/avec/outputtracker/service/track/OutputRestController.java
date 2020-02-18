0e7f6d05a862b3e0d57d412388c48d0abf0c775f
Avec <avec112@gmail.com>
2020-02-18 12:00:34 +0100

    - Fixed a security hole :-P Forgot to put REST-interface under security - Added /api to url /v1/output. Now it is /api/v1/output - Created a dumv rest api /api/v1/test (simple getter) - Did some cleaning inside pom.xml

--- src/main/java/io/avec/outputtracker/service/track/OutputRestController.java
+++ src/main/java/io/avec/outputtracker/service/track/OutputRestController.java
@@ -3,13 +3,12 @@ package io.avec.outputtracker.service.track;
 import io.avec.outputtracker.service.output.Output;
 import io.avec.outputtracker.service.output.OutputRepository;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @Slf4j
 @RestController
+@RequestMapping("/api/v1")
 public class OutputRestController {
 
     private final OutputRepository repository;
@@ -18,10 +17,15 @@ public class OutputRestController {
         this.repository = repository;
     }
 
-    @PostMapping("/v1/output")
+    @PostMapping("/output")
     public Output save(@RequestBody Output output) {
         log.debug("{}", output);
         return repository.save(output);
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        return "testing, testing";
     }
 
 
